bb0d5942362497ec8f15cc1cfe3abe75fed41acf
Walid Amriou <walidamriou@gmail.com>
2020-10-13 13:54:53 +0100

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/* 
+
+ESP32_Bluetooth_Low_Energy_BLE_Arduino
+Implement ESP32 Bluetooth Low Energy / BLE server for Espressif Systems ESP32 boards 
+by Arduino framework 
+
+Developed by: Walid Amriou
+Last update: October 2020
+
+*/
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
